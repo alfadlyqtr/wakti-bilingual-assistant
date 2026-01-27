c9f64f92b05e7e42e2e00ae8b7b1286b00952876
alfadlyqtr <alfadly@me.com>
2026-01-27 08:17:23 +0300

    w

--- src/components/business-card/BusinessCardBuilder.tsx
+++ src/components/business-card/BusinessCardBuilder.tsx
@@ -2111,7 +2111,10 @@ export const BusinessCardBuilder: React.FC<BusinessCardBuilderProps> = ({
 
         {/* Add to Apple Wallet */}
         <Button 
-          onClick={handleAddToWallet}
+          onClick={() => {
+            alert('clicked');
+            handleAddToWallet();
+          }}
           className="w-full h-12 bg-black text-white hover:bg-gray-900"
         >
           <Smartphone className="w-5 h-5 mr-2" />
